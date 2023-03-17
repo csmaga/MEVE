2ddfcc929e4e12ee41a812e52d7a89b9783787de
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-03-17 09:26:54 -0400

    Create TrimGalore_QC.sh

---
+++ TrimGalore_QC.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --partition=highmem_p
+#SBATCH --mail-user=christopher.smaga@uga.edu
+#SBATCH --mail-type=END,FAIL
+#SBATCH --job-name=MEVE_trimming
+#SBATCH --nodes=6
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=250G
+#SBATCH --time=48:00:00
+#SBATCH --output=/scratch/crs12448/MEVE/logs/trim.o
+#SBATCH --error=/scratch/crs12448/MEVE/logs/trim.e
+
+module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+DD="/scratch/crs12448/MEVE/Data/Raw"
+QC_OD="/scratch/crs12448/MEVE/QC/Trimmed_QC"
+Trim_OD="/scratch/crs12448/MEVE/Trimming/Trimmed_reads"
+# trim adapter sequences from corrected fastq files, setting strigency to 3 (only trimmed if at least 3 bases match adaptor sequence) based on STIM2014 (from MDH and SLB)
+# Create a for loop that goes into each directory, prints that directory, then does trimming on the two reads in that directory and fastqc, outputing fastqc results into QC_DD and trimmed reads into Trim_OD
+
+cd $DD
+
+for dir in $DD; do
+  echo "Starting $dir"
+  cd $dir
+  trim_galore --cores 6 --fastqc --fastqc_args "--outdir $QC_OD" -stringency 3 -o $Trim_OD --paired  *_1.fq.gz *_2.fq.gz
+  echo "Trimming of $dir finsihed."
+done
+
+module load FastQC/0.11.9-Java-11
+module load MultiQC/1.8-foss-2019b-Python-3.7.4
+
+cd $QC_OD
+multiqc /scratch/crs12448/MEVE/QC/Trimmed_QC
