edfad244f2e5fdda34a7b79bd771222220198378
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-03-17 09:32:07 -0400

    Update TrimGalore_QC.sh

--- TrimGalore_QC.sh
+++ TrimGalore_QC.sh
@@ -5,25 +5,30 @@
 #SBATCH --job-name=MEVE_trimming
 #SBATCH --nodes=6
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=250G
+#SBATCH --mem=120G
 #SBATCH --time=48:00:00
 #SBATCH --output=/scratch/crs12448/MEVE/logs/trim.o
 #SBATCH --error=/scratch/crs12448/MEVE/logs/trim.e
 
-module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+
 DD="/scratch/crs12448/MEVE/Data/Raw"
 QC_OD="/scratch/crs12448/MEVE/QC/Trimmed_QC"
 Trim_OD="/scratch/crs12448/MEVE/Trimming/Trimmed_reads"
-# trim adapter sequences from corrected fastq files, setting strigency to 3 (only trimmed if at least 3 bases match adaptor sequence) based on STIM2014 (from MDH and SLB)
-# Create a for loop that goes into each directory, prints that directory, then does trimming on the two reads in that directory and fastqc, outputing fastqc results into QC_DD and trimmed reads into Trim_OD
 
+
+module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+
+# Make the current directory the folder with raw data
 cd $DD
+
+# Create a for loop that goes into each directory, prints that directory, then does trimming on the two reads in that directory and fastqc, outputing fastqc results into QC_DD and trimmed reads into Trim_OD
+# Trim adapter sequences from fastq files, setting strigency to 3 (only trimmed if at least 3 bases match adaptor sequence), which is what was used for STIM2014, SLB's work, and PREE2. 
 
 for dir in $DD; do
   echo "Starting $dir"
   cd $dir
   trim_galore --cores 6 --fastqc --fastqc_args "--outdir $QC_OD" -stringency 3 -o $Trim_OD --paired  *_1.fq.gz *_2.fq.gz
-  echo "Trimming of $dir finsihed."
+  echo "Trimming of $dir finished."
 done
 
 module load FastQC/0.11.9-Java-11
