970d986e606997c97913cc85fdf97848072a4877
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-03-20 14:17:52 -0400

    Update TrimGalore_QC.sh

--- TrimGalore_QC.sh
+++ TrimGalore_QC.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
-#SBATCH --partition=highmem_p
+#SBATCH --partition=batch
 #SBATCH --mail-user=christopher.smaga@uga.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --job-name=MEVE_trimming
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=80G
+#SBATCH --mem=24G
 #SBATCH --time=48:00:00
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/trim.o
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/trim.e
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/trim_QC.o
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/trim_QC.e
 
 
-DD="/scratch/crs12448/MEVE/Data/Raw"
-QC_OD="/scratch/crs12448/MEVE/QC/Trimmed_QC"
-Trim_OD="/scratch/crs12448/MEVE/Trimming/Trimmed_reads"
+# DD="/scratch/crs12448/MEVE/Data/Raw"
+# QC_OD="/scratch/crs12448/MEVE/QC/Trimmed_QC"
+# Trim_OD="/scratch/crs12448/MEVE/Trimming/Trimmed_reads"
 
 
-module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+# module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 
-# Make the current directory the folder with raw data
-cd $DD
+# # Make the current directory the folder with raw data
+# cd $DD
 
-# Create a for loop that goes into each directory, prints that directory, then does trimming on the two reads in that directory and fastqc, outputing fastqc results into QC_DD and trimmed reads into Trim_OD
-# Trim adapter sequences from fastq files, setting strigency to 3 (only trimmed if at least 3 bases match adaptor sequence), which is what was used for STIM2014, SLB's work, and PREE2. 
+# # Create a for loop that goes into each directory, prints that directory, then does trimming on the two reads in that directory and fastqc, outputing fastqc results into QC_DD and trimmed reads into Trim_OD
+# # Trim adapter sequences from fastq files, setting strigency to 3 (only trimmed if at least 3 bases match adaptor sequence), which is what was used for STIM2014, SLB's work, and PREE2. 
 
-for dir in $DD/*; do
-  echo "Starting $dir"
-  cd $dir
-  trim_galore --cores 4 --fastqc --fastqc_args "--outdir $QC_OD" -stringency 3 -o $Trim_OD --paired  *_1.fq.gz *_2.fq.gz
-  echo "Trimming of $dir finished."
-done
+# for dir in $DD/*; do
+#   echo "Starting $dir"
+#   cd $dir
+#   trim_galore --cores 4 --fastqc --fastqc_args "--outdir $QC_OD" -stringency 3 -o $Trim_OD --paired  *_1.fq.gz *_2.fq.gz
+#   echo "Trimming of $dir finished."
+# done
 
 module load FastQC/0.11.9-Java-11
 module load MultiQC/1.8-foss-2019b-Python-3.7.4
 
 cd $QC_OD
 multiqc /scratch/crs12448/MEVE/QC/Trimmed_QC
+
