554a39250c2b0a8c7bf864c7d3097ac3c437e9ca
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-03-27 21:37:20 -0400

    Update HISAT2_alignment.sh

--- HISAT2_alignment.sh
+++ HISAT2_alignment.sh
@@ -12,57 +12,57 @@
 
 DD="/scratch/crs12448/MEVE/Trimming/Trimmed_reads"
 
-module load BEDTools/2.30.0-GCC-8.3.0
-ml HISAT2/2.2.1-foss-2019b
+# module load BEDTools/2.30.0-GCC-8.3.0
+# ml HISAT2/2.2.1-foss-2019b
 
-cd /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index
+# cd /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index
 
-# this code is based on MDH's STIMRNAseq github
-# using BEDTools to manually create exon and intron tables from the annotation file, which then can be used by hisat2 to build the genome index
-# Basically, calling out all exons (from the third column) and their positions, then converting to BED format. Then all genes from the 3rd column are called out and put into BED format.
-# To get intronic sights, exons are subtracted from gene coordinates.
-#
+# # this code is based on MDH's STIMRNAseq github
+# # using BEDTools to manually create exon and intron tables from the annotation file, which then can be used by hisat2 to build the genome index
+# # Basically, calling out all exons (from the third column) and their positions, then converting to BED format. Then all genes from the 3rd column are called out and put into BED format.
+# # To get intronic sights, exons are subtracted from gene coordinates.
+# #
 
-# First, take the 1, 4, and 5 columns from the annotation file for which the 3rd column is an exon
-awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4,$5}' /home/crs12448/ALL_METH_PROJ/Amiss.annot.2022.gff > Amiss.ref_exon.bed
-echo "Amiss.ref_exon.bed"
-head -n 10 Amiss.ref_exon.bed
+# # First, take the 1, 4, and 5 columns from the annotation file for which the 3rd column is an exon
+# awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4,$5}' /home/crs12448/ALL_METH_PROJ/Amiss.annot.2022.gff > Amiss.ref_exon.bed
+# echo "Amiss.ref_exon.bed"
+# head -n 10 Amiss.ref_exon.bed
 
-#Sort exons
-sortBed -i Amiss.ref_exon.bed > Amiss.ref_exon_temp.bed
+# #Sort exons
+# sortBed -i Amiss.ref_exon.bed > Amiss.ref_exon_temp.bed
 
-#Rename sorted file
-mv -f Amiss.ref_exon_temp.bed Amiss.ref_exon.bed
-echo "Amiss.ref_exon.bed"
-head -n 10 Amiss.ref_exon.bed
+# #Rename sorted file
+# mv -f Amiss.ref_exon_temp.bed Amiss.ref_exon.bed
+# echo "Amiss.ref_exon.bed"
+# head -n 10 Amiss.ref_exon.bed
 
-#Merge exons together that overlap
-mergeBed -i Amiss.ref_exon.bed > Amiss.ref_exon_merged.bed
-echo "Amiss.ref_exon_merged.bed"
-head -n 10 Amiss.ref_exon_merged.bed
-#
-# For lines where 3rd column is gene, extract columns 1,4,5,7 
-awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4,$5,$7}' /home/crs12448/ALL_METH_PROJ/Amiss.annot.2022.gff > Amiss.ref_gene.bed
-echo "Amiss.ref_gene.bed"
-head -n 10 Amiss.ref_gene.bed
+# #Merge exons together that overlap
+# mergeBed -i Amiss.ref_exon.bed > Amiss.ref_exon_merged.bed
+# echo "Amiss.ref_exon_merged.bed"
+# head -n 10 Amiss.ref_exon_merged.bed
+# #
+# # For lines where 3rd column is gene, extract columns 1,4,5,7 
+# awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4,$5,$7}' /home/crs12448/ALL_METH_PROJ/Amiss.annot.2022.gff > Amiss.ref_gene.bed
+# echo "Amiss.ref_gene.bed"
+# head -n 10 Amiss.ref_gene.bed
 
-#Sort
-sortBed -i Amiss.ref_gene.bed > Amiss.ref_gene_temp.bed
+# #Sort
+# sortBed -i Amiss.ref_gene.bed > Amiss.ref_gene_temp.bed
 
-#Rename
-mv -f Amiss.ref_gene_temp.bed Amiss.ref_gene.bed
-echo "Amiss.ref_gene.bed"
-head -n 10 Amiss.ref_gene.bed
+# #Rename
+# mv -f Amiss.ref_gene_temp.bed Amiss.ref_gene.bed
+# echo "Amiss.ref_gene.bed"
+# head -n 10 Amiss.ref_gene.bed
 
-# Subtract exons from gene coordinates to get introns 
-subtractBed -a Amiss.ref_gene.bed -b Amiss.ref_exon_merged.bed > Amiss.ref_intron.bed
-head -n 10 Amiss.ref_intron.bed
+# # Subtract exons from gene coordinates to get introns 
+# subtractBed -a Amiss.ref_gene.bed -b Amiss.ref_exon_merged.bed > Amiss.ref_intron.bed
+# head -n 10 Amiss.ref_intron.bed
 
 #######################################################################################################################################
 
 module load HISAT2/2.2.1-foss-2019b
 # # Build index genome using splice sites and exon files created above
-hisat2-build -p 10 --ss /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index/Amiss.ref_intron.bed --exon /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index/Amiss.ref_exon_merged.bed -f /home/crs12448/ALL_METH_PROJ/Amiss.ref.2022.fna /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index/Index/Amiss.index.ref.hisat2
+#hisat2-build -p 10 --ss /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index/Amiss.ref_intron.bed --exon /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index/Amiss.ref_exon_merged.bed -f /home/crs12448/ALL_METH_PROJ/Amiss.ref.2022.fna /scratch/crs12448/MEVE/Alignment/HISAT2/Genome_Index/Index/Amiss.index.ref.hisat2
 
 # For each file (S### is prefix), align the two reads resulting from that sample. Output file is SAM format.
 cd $DD 
