406515e7b7dbf0fa439a205deb85d7fafdc7e947
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-03-30 08:58:16 -0400

    Update GATK_SNPs.sh

--- GATK_SNPs.sh
+++ GATK_SNPs.sh
@@ -20,28 +20,44 @@ OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates"
 ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 
 # Run MarkDuplicates to mark duplicate reads from RNAseq data. For each file in the DD, perform the mark duplicates function
-cd $DD
-for i in *.bam;
-do
-gatk --java-options "-Xmx64G -XX:+UseParallelGC -XX:ParallelGCThreads=8" MarkDuplicates \
-      I=$i \
-      O=$OD/${i/.bam/_mark_dup.bam} \
-      M=$OD/${i/.bam/_mark_dup_metrics.txt}
-done
+#cd $DD
+# for i in *.bam;
+# do
+# gatk --java-options "-Xmx64G -XX:+UseParallelGC -XX:ParallelGCThreads=8" MarkDuplicates \
+#       I=$i \
+#       O=$OD/${i/.bam/_mark_dup.bam} \
+#       M=$OD/${i/.bam/_mark_dup_metrics.txt}
+# done
 
 ############################################################################################################################################3
 
 cd $OD
 OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads"
 
-# Run SplitNCigarReads to split reads that span introns into separate reads
-for i in *.bam;
-do
- gatk SplitNCigarReads \
-      -R /home/crs12448/ALL_METH_PROJ/Amiss.ref.2022.fna \
-      -I $i \
-      -O $OD_2/${i/_mark_dup.bam/_cigar.bam}
-done
+# # Run SplitNCigarReads to split reads that span introns into separate reads
+ for i in *.bam;
+ do
+  gatk SplitNCigarReads \
+       -R /scratch/crs12448/MEVE/Genome/Amiss.ref.2022.fasta \
+       -I $i \
+       -O $OD_2/${i/_mark_dup.bam/_cigar.bam}
+ done
 
 ############################################################################################################################################
+
+# cd $OD_2
+
+# OD_3="dfafdafda"
+
+# for i in *.bam;
+# do
+#  gatk --java-options "-Xmx64g -XX:+UseParallelGC -XX:ParallelGCThreads=8" HaplotypeCaller  \
+#    -R /home/crs12448/ALL_METH_PROJ/Amiss.ref.2022.fna \
+#    -I $i \
+#    -O $OD_3/${i/_cigar.bam/.g.vcf.gz} \
+#    -ERC GVCF
+#    -G StandardAnnotation \
+#    -G AS_StandardAnnotation
+# done
+ 
 
