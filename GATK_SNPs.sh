dac2be3a8b6c80c0844e5b017c58616129f53533
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-20 11:31:33 -0400

    Update GATK_SNPs.sh

--- GATK_SNPs.sh
+++ GATK_SNPs.sh
@@ -2,19 +2,19 @@
 #SBATCH --job-name=GATK_SNP                 # Job name
 #SBATCH --partition=highmem_p                            # Partition (queue) name
 #SBATCH --ntasks=1	                                # Single task job
-#SBATCH --cpus-per-task=8                       # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=200gb			                                # Total memory for job
-#SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK_genotype.o    # Standard output and error log - # replace cbergman with your myid
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK_genotype.e
+#SBATCH --cpus-per-task=12                       # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=300gb			                                # Total memory for job
+#SBATCH --time=72:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK_genotype_combined.o    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK_genotype_combined.e
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 # Directorty where aligned and sorted BAM files are located
-DD="/scratch/crs12448/MEVE/Alignment/HISAT2/BAM"
+#DD="/scratch/crs12448/MEVE/Alignment/HISAT2/BAM"
 
 # Set ouput directory
-OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates"
+#OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates"
 
 #Load the Genome Anlysis Toolkit
 ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
@@ -39,7 +39,7 @@ ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 # sbatch ~/MEVE/SplitCigar_4.sh
 
 # cd $OD
-OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads"
+#OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads"
 
 # # # Run SplitNCigarReads to split reads that span introns into separate reads
 #  for i in *.bam;
@@ -90,7 +90,7 @@ OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads"
 # From my understanding, this will allow genotypes to be called in samples even if they match the reference, as long as one sample contains a SNP in that position. 
 
 
-OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
+#OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
 # cd /scratch/crs12448/MEVE/GATK/FixBam
 
 # for i in *.bam;
@@ -124,13 +124,50 @@ OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
 #   vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.g.vcf
 # done
 
+############################################################################################################################################
+# Retry haplotype caller, this time on all samples at once - maybe this will change the high heterozygosity values I saw with the previous pipeline
 
+
+OD_5="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Combined_samples"
+cd /scratch/crs12448/MEVE/GATK/FixBam
+
+gatk --java-options "-Xmx300g -XX:+UseParallelGC -XX:ParallelGCThreads=12" HaplotypeCaller  \
+ -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+ -I S231_cigar_fix.bam S242_cigar_fix.bam S246_cigar_fix.bam S247_cigar_fix.bam S252_cigar_fix.bam S256_2_cigar_fix.bam S263_cigar_fix.bam S266_2_cigar_fix.bam S280_cigar_fix.bam S295_cigar_fix.bam S302_cigar_fix.bam S316_cigar_fix.bam \
+    S317_cigar_fix.bam S319_cigar_fix.bam S337_cigar_fix.bam S344_cigar_fix.bam S359_cigar_fix.bam S376_cigar_fix.bam S388_cigar_fix.bam S391_cigar_fix.bam S392_cigar_fix.bam S393_cigar_fix.bam S406_cigar_fix.bam S432_cigar_fix.bam \
+ --native-pair-hmm-threads 12 -O $OD_5/AP_WO_combined.vcf 
+
+
+
+
+#########################################################################
+# ## AGAIN this takes a LONG time, so split into several scripts
+# cd ~/MEVE
+
+# sbatch HaploCaller_1.sh
+# sbatch HaploCaller_2.sh
+# sbatch HaploCaller_3.sh
+# sbatch HaploCaller_4.sh
+
+
+# The GVCF files are HUGE because they count every base covered regardless of whether it is variable or not (so that genotyping cann be done with multiple individuals). Combining the raw files would take forever and lead to a massive GVCF, so first filter out low quality and low depth SNPs
+# Perform the filtering with vcftools
+# ml VCFtools/0.1.16-GCC-8.3.0-Perl-5.30.0
+
+# FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF"
+
+# cd /scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF
+# # This code only keeps sites that have a mean depth > 20, minimum quality of 30. No max depth set because in RNAseq, I expect some depths are really high biologically
+#  for i in S231 S242 S246 S247 S252 S256_2 S263 S266_2 S280 S295 S302 S316 S317 S319 S337 S344 S359 S376 S388 S391 S392 S393 S406 S432;
+#  do
+#   vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.g.vcf
+# done
 
 
 # Combine the GVCF files into one using the CombineGVCFs function. There is also a GenomicDBImport option that is better for handling more files, but I understand it less and think this might work for only 24 samples (plus they are filtered and have less SNPs):
  
- FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF"
- cd $FILTER_OD
+ #FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF"
+ #cd $FILTER_OD
  
 #  gatk --java-options "-Xmx200g -XX:+UseParallelGC -XX:ParallelGCThreads=8" CombineGVCFs \
 #    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
@@ -163,9 +200,29 @@ OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
 ######################################################
 
 # Now we have a single VCF with all samples. We need to genotype them all together now, which can be done using GenotypeGVCFs as below
-cd $FILTER_OD
+#cd $FILTER_OD
 
- gatk --java-options "-Xmx200g -XX:+UseParallelGC -XX:ParallelGCThreads=8" GenotypeGVCFs \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -V all_samples.g.vcf \
-   -O /scratch/crs12448/MEVE/GATK/GenotypeGVCF/AP_WO_SNPs.vcf
+ #gatk --java-options "-Xmx200g -XX:+UseParallelGC -XX:ParallelGCThreads=8" GenotypeGVCFs \
+  # -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+   #-V all_samples.g.vcf \
+   #-O /scratch/crs12448/MEVE/GATK/GenotypeGVCF/AP_WO_SNPs.vcf
+
+###################################################
+
+# Filter SNPs
+#vcftools --vcf AP_WO_SNPs.vcf --remove-indels --maf 0.1 --max-missing 0.8 --minQ 30 --min-meanDP 20 --minDP 20 --recode --stdout > AP_WO_SNPs_filter.vcf
+
+
+# Create a matrix of SNPs for exploratory analysis
+#bcftools query AP_WO_SNPs_filter.vcf -f '%CHROM\t%POS[\t%GT]\n' > snp_matrix.txt
+
+# Extract sample names
+#bcftools query -l AP_WO_SNPs_filter.vcf > sample_names.txt
+
+# Only keep loci with 2 alleles - went from 4871 to 4751 sites
+#vcftools --vcf AP_WO_SNPs_filter.vcf --min-alleles 2 --max-alleles 2 --recode --out AP_WO_SNPs_filter_maxtwo.vcf
+
+#vcftools --vcf AP_WO_SNPs_filter_maxtwo.vcf 
+
+
+#  Transfer those files to local machine for use in R
