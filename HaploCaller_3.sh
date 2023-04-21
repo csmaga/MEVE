f612ad00b4858d55bb87a9857f6d57ee835d9b09
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-21 14:15:29 -0400

    Update HaploCaller_3.sh

--- HaploCaller_3.sh
+++ HaploCaller_3.sh
@@ -5,19 +5,19 @@
 #SBATCH --cpus-per-task=4                        # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=120gb			                                # Total memory for job
 #SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK_haplo_3_2.o    # Standard output and error log - # replace cbergman with your myid
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK_haplo_3_2.e
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK_haplo2_3.o    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK_haplo2_3.e
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 
-OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
+OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF2"
 cd /scratch/crs12448/MEVE/GATK/FixBam
 
-for i in S302;
+for i in S317 S319 S337 S344 S359 S376;
 do
- gatk --java-options "-Xmx120g -XX:+UseParallelGC -XX:ParallelGCThreads=4" HaplotypeCaller  \
+ gatk --java-options "-Xmx120g" HaplotypeCaller  \
    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
    -I ${i}_cigar_fix.bam \
    -O $OD_4/${i/_cigar_fix.bam/.g.vcf.gz} \
