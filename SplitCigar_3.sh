74d64b9925f9f4707c36c771de62a03b291658ba
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-21 14:15:38 -0400

    Update SplitCigar_3.sh

--- SplitCigar_3.sh
+++ SplitCigar_3.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=GATK_SNP                 # Job name
 #SBATCH --partition=batch	                            # Partition (queue) name
 #SBATCH --ntasks=1	                                # Single task job
-#SBATCH --cpus-per-task=2                           # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --cpus-per-task=1                           # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=32gb			                                # Total memory for job
 #SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK_cigar_3.o    # Standard output and error log - # replace cbergman with your myid
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK_cigar_3.e
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK_cigar2_3.o    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK_cigar2_3.e
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
@@ -14,19 +14,19 @@
 DD="/scratch/crs12448/MEVE/Alignment/HISAT2/BAM"
 
 # Set ouput directory
-OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates"
+OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates2"
 
 #Load the Genome Anlysis Toolkit
 ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 ############################################################################################################################################3
 
 cd $OD
-OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads"
+OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads2"
 
 # # Run SplitNCigarReads to split reads that span introns into separate reads
  for i in S317 S319 S337 S344 S359 S376;
  do
-  gatk --java-options "-Xmx32G -XX:+UseParallelGC -XX:ParallelGCThreads=2"  SplitNCigarReads \
+  gatk --java-options "-Xmx32G"  SplitNCigarReads \
        -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
        -I ${i}_mark_dup.bam  \
        -O $OD_2/${i}_cigar.bam
