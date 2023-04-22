3ea921e5bebb91b123d771750846a0dc2f58e6a6
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-22 09:40:16 -0400

    Update GATK_SNPs_2.sh

--- GATK_SNPs_2.sh
+++ GATK_SNPs_2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=GATK_SNP                 # Job name
-#SBATCH --partition=highmem_p                            # Partition (queue) name
+#SBATCH --partition=bash                           # Partition (queue) name
 #SBATCH --ntasks=1	                                # Single task job
 #SBATCH --cpus-per-task=1                       # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=120gb			                                # Total memory for job
+#SBATCH --mem=64gb			                                # Total memory for job
 #SBATCH --time=72:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK2_dup.o    # Standard output and error log - # replace cbergman with your myid
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK2_dup_new.e
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK2_fix2.o    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK2_fix2.e
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
@@ -20,14 +20,14 @@ OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates2"
 ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 
 # Run MarkDuplicates to mark duplicate reads from RNAseq data. For each file in the DD, perform the mark duplicates function. 
-cd $DD
-for i in *.bam;
- do
- gatk --java-options "-Xmx120G" MarkDuplicates \
-       I=$i \
-       O=$OD/${i/.bam/_mark_dup.bam} \
-       M=$OD/${i/.bam/_mark_dup_metrics.txt}
- done
+# cd $DD
+# for i in *.bam;
+#  do
+#  gatk --java-options "-Xmx120G" MarkDuplicates \
+#        I=$i \
+#        O=$OD/${i/.bam/_mark_dup.bam} \
+#        M=$OD/${i/.bam/_mark_dup_metrics.txt}
+#  done
 
 ############################################################################################################################################3
 
@@ -39,7 +39,7 @@ for i in *.bam;
 # sbatch ~/MEVE/SplitCigar_4.sh
 
 # cd $OD
-#OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads"
+OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads2"
 
 # # # Run SplitNCigarReads to split reads that span introns into separate reads
 #  for i in *.bam;
@@ -54,33 +54,34 @@ for i in *.bam;
 # Call SNPs for each sample individually using haplotype caller in GATK. With the -ERC GVCF option, temporary .gvcf files are created that can then be merged into one, and the genotype calls can be made across all samples.
 # From my understanding, this will allow genotypes to be called in samples even if they match the reference, as long as one sample contains a SNP in that position. 
 
-#cd $OD_2
+# cd $OD_2
 
-#OD_3="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
+# OD_3="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
 
 # for i in *.bam;
 # do
 #  gatk --java-options "-Xmx120g -XX:+UseParallelGC -XX:ParallelGCThreads=8" HaplotypeCaller  \
 #    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-#    -I $i \
-#    -O $OD_3/${i/_cigar.bam/.g.vcf.gz} \
-#    -ERC GVCF
-# done
+#     -I $i \
+#     -O $OD_3/${i/_cigar.bam/.g.vcf.gz} \
+#     -ERC GVCF
+#  done
  # !!!!!!!!!!!!!!!!!  This gives me an error that I am trying to run on more than one sample. From a little googling, that may be because my BAM files are missing the @RG identifier. Sure enough,
  # when I look at the BAM files, there is no @RG. Luckily, GATK has a tool to add RG to samples with the below code. I don't think the information in RG is useful for my application,  so i only added
  # the required fields, which are RGLB (read group library), RGPU (read group platform unit?), RGPL (read group platform), and RGSM (read group sample - this is the important one). For all samples, I will leave all 
  # identifiers the same except for RG sample name. 
 
 # Fix the read groups......
-#cd $OD_2
+cd $OD_2
 
-# for i in *.bam;
-# do
-# gatk --java-options "-Xmx64g -XX:+UseParallelGC -XX:ParallelGCThreads=4" AddOrReplaceReadGroups \
-#         I=$i  O=/scratch/crs12448/MEVE/GATK/FixBam/${i/_cigar.bam/_cigar_fix.bam}  \
-#         SORT_ORDER=coordinate  RGLB=seq  RGPU=1 RGPL=illumina  RGSM=${i/_cigar.bam/}  \
-#         CREATE_INDEX=True
-# done
+for i in *.bam;
+ do
+ gatk --java-options "-Xmx64g" AddOrReplaceReadGroups \
+         I=$i  \
+         O=/scratch/crs12448/MEVE/GATK/FixBam2/${i/_cigar.bam/_cigar_fix.bam}  \
+         SORT_ORDER=coordinate  RGLB=seq  RGPU=1 RGPL=illumina  RGSM=${i/_cigar.bam/}  \
+         CREATE_INDEX=True
+ done
 
 
 
@@ -88,14 +89,15 @@ for i in *.bam;
 # Retry haplotype caller.....
 #Call SNPs for each sample individually using haplotype caller in GATK. With the -ERC GVCF option, temporary .gvcf files are created that can then be merged into one, and the genotype calls can be made across all samples.
 # From my understanding, this will allow genotypes to be called in samples even if they match the reference, as long as one sample contains a SNP in that position. 
-
+# !!!!!!!!!!!!!!!!!!!!!!!!!!
+####### I don't think multithreading works for this, which may be why I got such weird results from my first run through the entire pipeline. Trying to run everything again but without multiple threads to see if it fixes the issues.....HOPEFULLY!
 
 #OD_4="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF"
 # cd /scratch/crs12448/MEVE/GATK/FixBam
 
 # for i in *.bam;
 # do
-#  gatk --java-options "-Xmx120g -XX:+UseParallelGC -XX:ParallelGCThreads=4" HaplotypeCaller  \
+#  gatk --java-options "-Xmx120g" HaplotypeCaller  \
 #    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
 #    -I $i \
 #    -O $OD_4/${i/_cigar.bam/.g.vcf.gz} \
@@ -113,18 +115,24 @@ for i in *.bam;
 
 # The GVCF files are HUGE because they count every base covered regardless of whether it is variable or not (so that genotyping cann be done with multiple individuals). Combining the raw files would take forever and lead to a massive GVCF, so first filter out low quality and low depth SNPs
 # Perform the filtering with vcftools
+
 # ml VCFtools/0.1.16-GCC-8.3.0-Perl-5.30.0
 
-# FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF"
+# FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/FilterGVCF2"
 
-# cd /scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF
+# cd /scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF2
 # # This code only keeps sites that have a mean depth > 20, minimum quality of 30. No max depth set because in RNAseq, I expect some depths are really high biologically
 #  for i in S231 S242 S246 S247 S252 S256_2 S263 S266_2 S280 S295 S302 S316 S317 S319 S337 S344 S359 S376 S388 S391 S392 S393 S406 S432;
 #  do
-#   vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.g.vcf
+#   vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.vcf
 # done
 
 ############################################################################################################################################
+
+                                        # DO NOT NEED TO RUN THIS BLOCK OF CODE #
+
+############################################################################################################################################
+
 # Retry haplotype caller, this time on all samples at once - maybe this will change the high heterozygosity values I saw with the previous pipeline
 
 
@@ -163,39 +171,40 @@ for i in *.bam;
 #   vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.g.vcf
 # done
 
+##############################################################################################################################################################################################################################################################################
 
 # Combine the GVCF files into one using the CombineGVCFs function. There is also a GenomicDBImport option that is better for handling more files, but I understand it less and think this might work for only 24 samples (plus they are filtered and have less SNPs):
  
- #FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF"
+ #FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF2"
  #cd $FILTER_OD
  
 #  gatk --java-options "-Xmx200g -XX:+UseParallelGC -XX:ParallelGCThreads=8" CombineGVCFs \
 #    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-#    --variant S231_filtered.g.vcf \
-#    --variant S242_filtered.g.vcf \
-#    --variant S246_filtered.g.vcf \
-#    --variant S247_filtered.g.vcf \
-#    --variant S252_filtered.g.vcf \
-#    --variant S256_2_filtered.g.vcf \
-#    --variant S263_filtered.g.vcf \
-#    --variant S266_2_filtered.g.vcf \
-#    --variant S280_filtered.g.vcf \
-#    --variant S295_filtered.g.vcf \
-#    --variant S302_filtered.g.vcf \
-#    --variant S316_filtered.g.vcf \
-#    --variant S317_filtered.g.vcf \
-#    --variant S319_filtered.g.vcf \
-#    --variant S337_filtered.g.vcf \
-#    --variant S344_filtered.g.vcf \
-#    --variant S359_filtered.g.vcf \
-#    --variant S376_filtered.g.vcf \
-#    --variant S388_filtered.g.vcf \
-#    --variant S391_filtered.g.vcf \
-#    --variant S392_filtered.g.vcf \
-#    --variant S393_filtered.g.vcf \
-#    --variant S406_filtered.g.vcf \
-#    --variant S432_filtered.g.vcf \
-#    -O all_samples.g.vcf
+#    --variant S231_filtered.vcf \
+#    --variant S242_filtered.vcf \
+#    --variant S246_filtered.vcf \
+#    --variant S247_filtered.vcf \
+#    --variant S252_filtered.vcf \
+#    --variant S256_2_filtered.vcf \
+#    --variant S263_filtered.vcf \
+#    --variant S266_2_filtered.vcf \
+#    --variant S280_filtered.vcf \
+#    --variant S295_filtered.vcf \
+#    --variant S302_filtered.vcf \
+#    --variant S316_filtered.vcf \
+#    --variant S317_filtered.vcf \
+#    --variant S319_filtered.vcf \
+#    --variant S337_filtered.vcf \
+#    --variant S344_filtered.vcf \
+#    --variant S359_filtered.vcf \
+#    --variant S376_filtered.vcf \
+#    --variant S388_filtered.vcf \
+#    --variant S391_filtered.vcf \
+#    --variant S392_filtered.vcf \
+#    --variant S393_filtered.vcf \
+#    --variant S406_filtered.vcf \
+#    --variant S432_filtered.vcf \
+#    -O all_samples.vcf
 
 ######################################################
 
