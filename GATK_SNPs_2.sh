39905059147d957c00a28973805a75960426626b
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-26 16:50:05 -0400

    Update GATK_SNPs_2.sh

--- GATK_SNPs_2.sh
+++ GATK_SNPs_2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=GATK_SNP                 # Job name
-#SBATCH --partition=batch                          # Partition (queue) name
+#SBATCH --partition=highmem_p                         # Partition (queue) name
 #SBATCH --ntasks=1	                                # Single task job
 #SBATCH --cpus-per-task=1                       # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=60gb			                                # Total memory for job
+#SBATCH --mem=200gb			                                # Total memory for job
 #SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK2_fix2.o    # Standard output and error log - # replace cbergman with your myid
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK2_fix2.e
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK2_filter_combine.o    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK2_filter_combine.e
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 # Directorty where aligned and sorted BAM files are located
-DD="/scratch/crs12448/MEVE/Alignment/HISAT2/BAM"
+#DD="/scratch/crs12448/MEVE/Alignment/HISAT2/BAM"
 
 # Set ouput directory
-OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates2"
+#OD="/scratch/crs12448/MEVE/GATK/MarkDuplicates2"
 
 #Load the Genome Anlysis Toolkit
 ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
@@ -39,7 +39,7 @@ ml  GATK/4.3.0.0-GCCcore-8.3.0-Java-1.8
 # sbatch ~/MEVE/SplitCigar_4.sh
 
 # cd $OD
-OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads2"
+#OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads2"
 
 # # # Run SplitNCigarReads to split reads that span introns into separate reads
 #  for i in *.bam;
@@ -72,16 +72,16 @@ OD_2="/scratch/crs12448/MEVE/GATK/SplitNCigarReads2"
  # identifiers the same except for RG sample name. 
 
 # Fix the read groups......
-cd $OD_2
+# cd $OD_2
 
-for i in *.bam;
- do
- gatk --java-options "-Xmx60g" AddOrReplaceReadGroups \
-         I=$i  \
-         O=/scratch/crs12448/MEVE/GATK/FixBam2/${i/_cigar.bam/_cigar_fix.bam}  \
-         SORT_ORDER=coordinate  RGLB=seq  RGPU=1 RGPL=illumina  RGSM=${i/_cigar.bam/}  \
-         CREATE_INDEX=True
- done
+# for i in *.bam;
+#  do
+#  gatk --java-options "-Xmx60g" AddOrReplaceReadGroups \
+#          I=$i  \
+#          O=/scratch/crs12448/MEVE/GATK/FixBam2/${i/_cigar.bam/_cigar_fix.bam}  \
+#          SORT_ORDER=coordinate  RGLB=seq  RGPU=1 RGPL=illumina  RGSM=${i/_cigar.bam/}  \
+#          CREATE_INDEX=True
+#  done
 
 
 
@@ -116,16 +116,16 @@ for i in *.bam;
 # The GVCF files are HUGE because they count every base covered regardless of whether it is variable or not (so that genotyping cann be done with multiple individuals). Combining the raw files would take forever and lead to a massive GVCF, so first filter out low quality and low depth SNPs
 # Perform the filtering with vcftools
 
-# ml VCFtools/0.1.16-GCC-8.3.0-Perl-5.30.0
+ml VCFtools/0.1.16-GCC-8.3.0-Perl-5.30.0
 
-# FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/FilterGVCF2"
+FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/FilterGVCF2"
 
-# cd /scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF2
+cd /scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF2
 # # This code only keeps sites that have a mean depth > 20, minimum quality of 30. No max depth set because in RNAseq, I expect some depths are really high biologically
-#  for i in S231 S242 S246 S247 S252 S256_2 S263 S266_2 S280 S295 S302 S316 S317 S319 S337 S344 S359 S376 S388 S391 S392 S393 S406 S432;
-#  do
-#   vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.vcf
-# done
+ for i in S231 S242 S246 S247 S252 S256_2 S263 S266_2 S280 S295 S302 S316 S317 S319 S337 S344 S359 S376 S388 S391 S392 S393 S406 S432;
+do
+  vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.vcf
+done
 
 ############################################################################################################################################
 
@@ -175,36 +175,36 @@ for i in *.bam;
 
 # Combine the GVCF files into one using the CombineGVCFs function. There is also a GenomicDBImport option that is better for handling more files, but I understand it less and think this might work for only 24 samples (plus they are filtered and have less SNPs):
  
- #FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/Filter_GVCF2"
- #cd $FILTER_OD
+ FILTER_OD="/scratch/crs12448/MEVE/GATK/HaplotypeCaller/FilterGVCF2"
+ cd $FILTER_OD
  
-#  gatk --java-options "-Xmx200g -XX:+UseParallelGC -XX:ParallelGCThreads=8" CombineGVCFs \
-#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-#    --variant S231_filtered.vcf \
-#    --variant S242_filtered.vcf \
-#    --variant S246_filtered.vcf \
-#    --variant S247_filtered.vcf \
-#    --variant S252_filtered.vcf \
-#    --variant S256_2_filtered.vcf \
-#    --variant S263_filtered.vcf \
-#    --variant S266_2_filtered.vcf \
-#    --variant S280_filtered.vcf \
-#    --variant S295_filtered.vcf \
-#    --variant S302_filtered.vcf \
-#    --variant S316_filtered.vcf \
-#    --variant S317_filtered.vcf \
-#    --variant S319_filtered.vcf \
-#    --variant S337_filtered.vcf \
-#    --variant S344_filtered.vcf \
-#    --variant S359_filtered.vcf \
-#    --variant S376_filtered.vcf \
-#    --variant S388_filtered.vcf \
-#    --variant S391_filtered.vcf \
-#    --variant S392_filtered.vcf \
-#    --variant S393_filtered.vcf \
-#    --variant S406_filtered.vcf \
-#    --variant S432_filtered.vcf \
-#    -O all_samples.vcf
+ gatk --java-options "-Xmx200g" CombineGVCFs \
+   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+   --variant S231_filtered.vcf \
+   --variant S242_filtered.vcf \
+   --variant S246_filtered.vcf \
+   --variant S247_filtered.vcf \
+   --variant S252_filtered.vcf \
+   --variant S256_2_filtered.vcf \
+   --variant S263_filtered.vcf \
+   --variant S266_2_filtered.vcf \
+   --variant S280_filtered.vcf \
+   --variant S295_filtered.vcf \
+   --variant S302_filtered.vcf \
+   --variant S316_filtered.vcf \
+   --variant S317_filtered.vcf \
+   --variant S319_filtered.vcf \
+   --variant S337_filtered.vcf \
+   --variant S344_filtered.vcf \
+   --variant S359_filtered.vcf \
+   --variant S376_filtered.vcf \
+   --variant S388_filtered.vcf \
+   --variant S391_filtered.vcf \
+   --variant S392_filtered.vcf \
+   --variant S393_filtered.vcf \
+   --variant S406_filtered.vcf \
+   --variant S432_filtered.vcf \
+   -O all_samples.vcf
 
 ######################################################
 
