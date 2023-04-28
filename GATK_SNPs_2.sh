013f64b7c8ded38096c793f8bfc6c494383678c6
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-28 09:51:17 -0400

    Update GATK_SNPs_2.sh

--- GATK_SNPs_2.sh
+++ GATK_SNPs_2.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=1                       # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=200gb			                                # Total memory for job
 #SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK2_filter_combine.o    # Standard output and error log - # replace cbergman with your myid
-#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK2_filter_combine.e
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/GATK2_combine.o    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --error=/scratch/crs12448/MEVE/Logs/GATK2_combine.e
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
