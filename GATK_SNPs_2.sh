9c7edc20863550fa350c9cfaf7bbf4f7e847881c
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-04-28 09:34:26 -0400

    Update GATK_SNPs_2.sh

--- GATK_SNPs_2.sh
+++ GATK_SNPs_2.sh
@@ -124,7 +124,7 @@ cd /scratch/crs12448/MEVE/GATK/HaplotypeCaller/GVCF2
 # # This code only keeps sites that have a mean depth > 20, minimum quality of 30. No max depth set because in RNAseq, I expect some depths are really high biologically
  for i in S231 S242 S246 S247 S252 S256_2 S263 S266_2 S280 S295 S302 S316 S317 S319 S337 S344 S359 S376 S388 S391 S392 S393 S406 S432;
 do
-  vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --stdout > $FILTER_OD/${i}_filtered.vcf
+  vcftools --vcf $i --minQ 30 --min-meanDP 20 --recode --recode-INFO-all --stdout > $FILTER_OD/${i}_filtered.vcf
 done
 
 ############################################################################################################################################
