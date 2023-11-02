0b64f347092dbd7fe198dcf0bbe259378b9bb1d1
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-11-02 17:20:56 -0400

    Update GATK_RNASEQ_prep.sh

--- scripts/GATK_RNASEQ_prep.sh
+++ scripts/GATK_RNASEQ_prep.sh
@@ -24,38 +24,41 @@ ml GATK/4.4.0.0-GCCcore-11.3.0-Java-17
 
 # Run MarkDuplicates to mark duplicate reads from RNAseq data. For each file in the DD, perform the mark duplicates function. Calling on JAVA with 64GB and 8 cores
 
-mkdir $OUTDIR/GATK/MarkDuplicates
+#mkdir $OUTDIR/GATK/MarkDuplicates
 
- gatk MarkDuplicates \
-     I=$OUTDIR/Alignment/HISAT2/BAM/${sample}.bam \
-     O=$OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup.bam \
-     M=$OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup_metrics.txt}
+#  gatk MarkDuplicates \
+#      I=$OUTDIR/Alignment/HISAT2/BAM/${sample}.bam \
+#      O=$OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup.bam \
+#      M=$OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup_metrics.txt}
 
-# SplitNCigar reads to split reads at "N"s (new read to the left and to the right of the N). Not sure exactly why this is necessary....
- mkdir $OUTDIR/GATK/SplitNCigarReads
+# # SplitNCigar reads to split reads at "N"s (new read to the left and to the right of the N). Not sure exactly why this is necessary....
+#  mkdir $OUTDIR/GATK/SplitNCigarReads
 
- gatk SplitNCigarReads \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -I $OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup.bam  \
-   -O $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam
+#  gatk SplitNCigarReads \
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -I $OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup.bam  \
+#    -O $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam
 
-# # Recalibration of base quality
-mkdir $OUTDIR/GATK/Recalibration
+# # Recalibration of base quality - This REQUIRES known variable sites. Basically, because quality of base calls is so important in the variantn calling process, and even at 99.9% quality, translates to a lot of error, this function uses machine learning to determine if there are 
+# patterns of base calls that might suggest errors. Since I do not have a known-variant file, need to first call variants on the processed BAM files, then filter and use that VCF file as known sites for which this can run. The GATK toolkit suggests doing this iteratively until results are 
+# robust. Not quite sure yet what they will look like but will play around with it. 
 
-gatk BaseRecalibrator \
-   -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -O $OUTDIR/GATK/Recalibration/recal_data.table
+#mkdir $OUTDIR/GATK/Recalibration
 
-gatk ApplyBQSR \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
-   --bqsr-recal-file $OUTDIR/GATK/Recalibration/recal_data.table \
-   -O $OUTDIR/GATK/Recalibration/${sample}_recal.bam
+# gatk BaseRecalibrator \
+#    -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -O $OUTDIR/GATK/Recalibration/recal_data.table
+
+# gatk ApplyBQSR \
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
+#    --bqsr-recal-file $OUTDIR/GATK/Recalibration/recal_data.table \
+#    -O $OUTDIR/GATK/Recalibration/${sample}_recal.bam
  
-gatk AnalyzeCovariates \
-  -bqsr $OUTDIR/GATK/Recalibration/recal_data.table \
-  -plots AnalyzeCovariates.pdf
+# gatk AnalyzeCovariates \
+#   -bqsr $OUTDIR/GATK/Recalibration/recal_data.table \
+#   -plots AnalyzeCovariates.pdf
 
 # # Variant calling 
 
@@ -64,7 +67,7 @@ mkdir mkdir $OUTDIR/GATK/HaplotypeCaller
 
 gatk --java-options "-Xmx4g" HaplotypeCaller  \
   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-  -I $OUTDIR/GATK/Recalibration/${sample}_recal.bam \
+  -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
   -O $OUTDIR/GATK/HaplotypeCaller/${sample}.g.vcf.gz \
   -ERC GVCF
 
