e604984cab205c7c284ad5bf00a7cd832a956e23
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-11-02 13:47:17 -0400

    Update GATK_RNASEQ_prep.sh

--- scripts/GATK_RNASEQ_prep.sh
+++ scripts/GATK_RNASEQ_prep.sh
@@ -32,40 +32,40 @@ mkdir $OUTDIR/GATK/MarkDuplicates
      M=$OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup_metrics.txt}
 
 # SplitNCigar reads to split reads at "N"s (new read to the left and to the right of the N). Not sure exactly why this is necessary....
- mkdir $OUTDIR/GATK/SplitNCigarReads
+#  mkdir $OUTDIR/GATK/SplitNCigarReads
 
- gatk SplitNCigarReads \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -I $OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup.bam  \
-   -O $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam
+#  gatk SplitNCigarReads \
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -I $OUTDIR/GATK/MarkDuplicates/${sample}_mark_dup.bam  \
+#    -O $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam
 
-# Recalibration of base quality
-mkdir $OUTDIR/GATK/Recalibration
+# # Recalibration of base quality
+# mkdir $OUTDIR/GATK/Recalibration
 
-gatk BaseRecalibrator \
-   -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -O $OUTDIR/GATK/Recalibration/recal_data.table
+# gatk BaseRecalibrator \
+#    -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -O $OUTDIR/GATK/Recalibration/recal_data.table
 
-gatk ApplyBQSR \
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
-   --bqsr-recal-file $OUTDIR/GATK/Recalibration/recal_data.table \
-   -O $OUTDIR/GATK/Recalibration/${sample}_recal.bam
+# gatk ApplyBQSR \
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -I $OUTDIR/GATK/SplitNCigarReads/${sample}_cigar.bam \
+#    --bqsr-recal-file $OUTDIR/GATK/Recalibration/recal_data.table \
+#    -O $OUTDIR/GATK/Recalibration/${sample}_recal.bam
  
-gatk AnalyzeCovariates \
-  -bqsr $OUTDIR/GATK/Recalibration/recal_data.table \
-  -plots AnalyzeCovariates.pdf
+# gatk AnalyzeCovariates \
+#   -bqsr $OUTDIR/GATK/Recalibration/recal_data.table \
+#   -plots AnalyzeCovariates.pdf
 
-# Variant calling 
+# # Variant calling 
 
-#Make directory for vcf files
-mkdir mkdir $OUTDIR/GATK/HaplotypeCaller
+# #Make directory for vcf files
+# mkdir mkdir $OUTDIR/GATK/HaplotypeCaller
 
-gatk --java-options "-Xmx4g" HaplotypeCaller  \
-  -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-  -I $OUTDIR/GATK/Recalibration/${sample}_recal.bam \
-  -O $OUTDIR/GATK/HaplotypeCaller/${sample}.g.vcf.gz \
-  -ERC GVCF
+# gatk --java-options "-Xmx4g" HaplotypeCaller  \
+#   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#   -I $OUTDIR/GATK/Recalibration/${sample}_recal.bam \
+#   -O $OUTDIR/GATK/HaplotypeCaller/${sample}.g.vcf.gz \
+#   -ERC GVCF
 
 
