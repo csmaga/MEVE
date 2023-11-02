3ab996b1c3592431e55a4305cf4ea39ee6db7369
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-11-02 10:25:37 -0400

    Create runR.sh

---
+++ scripts/runR.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=MEVE_count_reads		                        # Job name
+#SBATCH --partition=batch	                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=1		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=64gb			                                # Total memory for job
+#SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j	    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
+#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+cd /scratch/crs12448/MEVE/R
+
+ml R/4.3.1-foss-2022a
+
+R CMD BATCH ~/MEVE/GenomicFeatures_count_reads.r
