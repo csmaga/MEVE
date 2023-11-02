5a28ba8aac8957c4e5cba4b9ec8e0c0347e6b370
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-11-02 09:06:30 -0400

    Create alignment_stats.sh

---
+++ scripts/alignment_stats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=GAlignment_stats		                        # Job name
+#SBATCH --partition=batch	                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=1		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=32gb			                                # Total memory for job
+#SBATCH --time=24:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j	    
+#SBATCH --mail-user=christopher.smaga@uga.edu               
+#SBATCH --mail-type=END,FAIL                            
+
+cd "/scratch/crs12448/MEVE/Alignment/HISAT2/BAM"
+
+ml  SAMtools/1.16.1-GCC-11.3.0
+
+samtools flagstat *.bam > /scratch/crs12448/MEVE/Alignment/Stats/*_stats
+
