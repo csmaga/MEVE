c06f1790e14e178b33b9d1f57f708ee37974131b
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-12-13 15:19:31 -0500

    Update differential_coexpression.R

--- scripts/differential_coexpression.R
+++ scripts/differential_coexpression.R
@@ -11,13 +11,13 @@ norm_counts_AP<-as.matrix(norm_counts[c(1,2,5,7,9,11,14,15,17,19,20,21),])
 norm_counts_WO<-as.matrix(norm_counts[-c(1,2,5,7,9,11,14,15,17,19,20,21),])
 
 csd_results <- run_csd(
-  x_1 = norm_counts_AP, x_2 = norm_counts_WO,
-  n_it = 10, nThreads = 4L, verbose = FALSE
+  x_1 = norm_counts_WO, x_2 = norm_counts_AP,
+  n_it = 100, nThreads = 4L, verbose = FALSE
 )
 
 write.csv(csd_results, "csd_results.csv")
 
-pairs_to_pick <- 100L
+pairs_to_pick <- 1000L
 c_filter <- partial_argsort(csd_results$cVal, pairs_to_pick)
 c_frame <- csd_results[c_filter, ]
 s_filter <- partial_argsort(csd_results$sVal, pairs_to_pick)
