b9fa574f3797e1984f9a93282a68d9e35be6da23
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-12-13 16:20:19 -0500

    Update runR.sh

--- scripts/runR.sh
+++ scripts/runR.sh
@@ -5,14 +5,14 @@
 #SBATCH --cpus-per-task=4		                            # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=300gb			                                # Total memory for job
 #SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/scratch/crs12448/AP_WO_21/Diff_Coex/logs/log.%j	    # Standard output and error log - # replace cbergman with your myid
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j	    # Standard output and error log - # replace cbergman with your myid
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
-cd /scratch/crs12448/AP_WO_21/R
+cd /scratch/crs12448/MEVE/R
 
 ml R/4.3.1-foss-2022a
 
 export R_LIBS=/home/crs12448/R/x86_64-pc-linux-gnu-library/4.3
 
-R CMD BATCH ~/MEVE/scripts/differential_coexpression.R
+R CMD BATCH ~/MEVE/WGCNA_networks_cluster.R
