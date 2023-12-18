4fa805d39bbda4cab37ae9aeff7d8a04e474a5e4
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2023-12-18 16:33:37 -0500

    Update GATK_RNASEQ_prep.sh

--- scripts/GATK_RNASEQ_prep.sh
+++ scripts/GATK_RNASEQ_prep.sh
@@ -250,7 +250,7 @@ cd $OUTDIR/GATK/GenotypeGVCFs
 #vcftools --gvcf MEVE_variants_unfiltered.vcf.gz --remove-indels --max-missing 0.85 --recode --stdout > Filtered/MEVE_SNPs_filter_depth_m85.vcf
 
  gatk IndexFeatureFile \
-     -F MEVE_variants_unfiltered.vcf.gz
+     --feature-file MEVE_variants_unfiltered.vcf.gz
 
 
 gatk VariantFiltration -V MEVE_variants_unfiltered.vcf.gz  --filter-expression "QD < 2.0" --filter-name "QD2" \
