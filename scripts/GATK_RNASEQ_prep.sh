c1a7a6aae47f5176304b0fd508593f06f01e265b
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-02-05 11:55:57 -0500

    Update GATK_RNASEQ_prep.sh

--- scripts/GATK_RNASEQ_prep.sh
+++ scripts/GATK_RNASEQ_prep.sh
@@ -80,10 +80,6 @@ ml GATK/4.4.0.0-GCCcore-11.3.0-Java-17
 #   -O $OUTDIR/GATK/HaplotypeCaller/${sample}.g.vcf.gz \
 #   -ERC GVCF
 
-# Filter SNPs (hard filtering to reduce the size of the files). Otherwise, the combined file will be HUGE.
-# I don't think this should be bad because I am hard filtering out any variants that have poor read depth
-# or quality.
-
 #Change directory to where files are
 #cd $OUTDIR/GATK/HaplotypeCaller/
 
@@ -93,72 +89,12 @@ ml GATK/4.4.0.0-GCCcore-11.3.0-Java-17
 #Load modules
 #ml VCFtools/0.1.16-GCC-11.2.0
 
-#Filter each sample so the minimum read depth is 20 and quality score is 30. This should reduce the total
-# number of sites to a more manageable level before genotyping
-#vcftools --gzvcf ${sample}.g.vcf.gz --minQ 30 --min-meanDP 20 --recode --stdout > $OUTDIR/GATK/HaplotypeCaller/Filter_1/${sample}_filtered.g.vcf
-
-# Now that we have done a first pass filter, combine the GVCF files to perform genotyping across all individuals. Here, I am removing the two libraries with limited reads (S256 and S266). The _2 are the new ones with high coverage.
-
 # #Combine GVCFs
 # cd $OUTDIR/GATK/HaplotypeCaller/Filter_1
 # mkdir $OUTDIR/GATK/GenotypeGVCFs
 
-# gatk CombineGVCFs \
-#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-#  --variant S231_filtered.g.vcf \
-#  --variant S242_filtered.g.vcf \
-#  --variant S246_filtered.g.vcf \
-#  --variant S247_filtered.g.vcf \
-#  --variant S252_filtered.g.vcf \
-#  --variant S256_2_filtered.g.vcf \
-#  --variant S263_filtered.g.vcf \
-#  --variant S266_2_filtered.g.vcf \
-#  --variant S280_filtered.g.vcf \
-#  --variant S295_filtered.g.vcf \
-#  --variant S302_filtered.g.vcf \
-#  --variant S303_filtered.g.vcf \
-#  --variant S314_filtered.g.vcf \
-#  --variant S316_filtered.g.vcf \
-#  --variant S317_filtered.g.vcf \
-#  --variant S319_filtered.g.vcf \
-#  --variant S328_filtered.g.vcf \
-#  --variant S336_filtered.g.vcf \
-#  --variant S337_filtered.g.vcf \
-#  --variant S338_filtered.g.vcf \
-#  --variant S344_filtered.g.vcf \
-#  --variant S345_filtered.g.vcf \
-#  --variant S348_filtered.g.vcf \
-#  --variant S350_filtered.g.vcf \
-#  --variant S353_filtered.g.vcf \
-#  --variant S357_filtered.g.vcf \
-#  --variant S359_filtered.g.vcf \
-#  --variant S367_filtered.g.vcf \
-#  --variant S376_filtered.g.vcf \
-#  --variant S380_filtered.g.vcf \
-#  --variant S384_filtered.g.vcf \
-#  --variant S388_filtered.g.vcf \
-#  --variant S391_filtered.g.vcf \
-#  --variant S392_filtered.g.vcf \
-#  --variant S393_filtered.g.vcf \
-#  --variant S406_filtered.g.vcf \
-#  --variant S407_filtered.g.vcf \
-#  --variant S408_filtered.g.vcf \
-#  --variant S416_filtered.g.vcf \
-#  --variant S420_filtered.g.vcf \
-#  --variant S421_filtered.g.vcf \
-#  --variant S422_filtered.g.vcf \
-#  --variant S425_filtered.g.vcf \
-#  --variant S426_filtered.g.vcf \
-#  --variant S427_filtered.g.vcf \
-#  --variant S432_filtered.g.vcf \
-#  --variant S433_filtered.g.vcf \
-#  --variant S435_filtered.g.vcf \
-#  -O $OUTDIR/GATK/GenotypeGVCFs/all_samples.g.vcf
-
-
 #Change directory to where files are
 #cd $OUTDIR/GATK/HaplotypeCaller/
-
 
 # Combine the GVCF files to perform genotyping across all individuals. I am NOT filtering beforehand
 # # Here, I am removing the two libraries with limited reads (S256 and S266). The _2 are the new ones with high coverage.
@@ -214,47 +150,19 @@ ml GATK/4.4.0.0-GCCcore-11.3.0-Java-17
 #  --variant S435.g.vcf.gz \
 #  -O $OUTDIR/GATK/CombineGVCFs/all_samples_unfiltered.g.vcf.gz
  # Now we have a single VCF with all samples. We need to genotype them all together now, which can be done using GenotypeGVCFs as below
-cd $OUTDIR/GATK/GenotypeGVCFs
+# cd $OUTDIR/GATK/GenotypeGVCFs
 
-  gatk GenotypeGVCFs --java-options "-Xmx32g"\
-   -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
-   -V $OUTDIR/GATK/CombineGVCFs/all_samples_unfiltered.g.vcf.gz \
-   -L $OUTDIR/PopGen/gene_bed_sorted.bed -ip 100 \
-   -all-sites \
-   -O MEVE_variants_unfiltered_allgenes.vcf
+#   gatk GenotypeGVCFs --java-options "-Xmx32g"\
+#    -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta \
+#    -V $OUTDIR/GATK/CombineGVCFs/all_samples_unfiltered.g.vcf.gz \
+#    -L $OUTDIR/PopGen/gene_bed_sorted.bed -ip 100 \
+#    -all-sites \
+#    -O MEVE_variants_unfiltered_allgenes.vcf
 
 # Filter SNPs
 
-
-#vcftools --gvcf MEVE_variants_unfiltered.g.vcf --remove-indels --maf 0.1 --max-missing 0.9 --minQ 30 --min-meanDP 10 --minDP 10 --recode --stdout > $OUTDIR/Filtered/MEVE_SNPs_filter_1.vcf
-#vcftools --gvcf MEVE_variants_unfiltered.g.vcf --remove-indels --maf 0.05 --max-missing 0.9 --minQ 30 --min-meanDP 10 --minDP 10 --recode --stdout > Filtered/MEVE_SNPs_filter_2.vcf
-#vcftools --gvcf MEVE_variants_unfiltered.g.vcf --remove-indels --maf 0.1 --max-missing 0.95 --minQ 30 --min-meanDP 10 --minDP 10 --recode --stdout > $OUTDIR/Filtered/MEVE_SNPs_filter_3.vcf
-#vcftools --gvcf MEVE_variants_unfiltered.g.vcf --remove-indels --maf 0.05 --max-missing 0.95 --minQ 30 --min-meanDP 10 --minDP 10 --recode --stdout > $OUTDIR/Filtered/MEVE_SNPs_filter_4.vcf
-#vcftools --gvcf MEVE_variants_unfiltered.g.vcf --remove-indels --maf 0.1 --max-missing 0.85 --minQ 30 --min-meanDP 10 --minDP 10 --recode --stdout > $OUTDIR/Filtered/MEVE_SNPs_filter_5.vcf
-#vcftools --gvcf MEVE_variants_unfiltered.g.vcf --remove-indels --maf 0.05 --max-missing 0.85 --minQ 30 --min-meanDP 10 --minDP 10 --recode --stdout > $OUTDIR/Filtered/MEVE_SNPs_filter_6.vcf
-
-
-# First, filter by depth - only retain sites (SNPs) that have a quality above 30 (all of them in this case) and min mean depth of 10.
-#vcftools --gzvcf MEVE_SNPs_filter_depth.vcf --remove-indels --min-meanDP 10 --recode --stdout > Filtered/MEVE_SNPs_filter_depth_sdpth.vcf # 180443 sites remaining
-
-
-#vcftools --gvcf MEVE_variants_unfiltered.vcf.gz --remove-indels --minQ 30 --minDP 10 --recode --stdout > Filtered/MEVE_SNPs_filter_depth.vcf
-#bcftools query -f "A\n" MEVE_SNPs_filter_depth.vcf | wc -l
-
-# 1561991 sites retained
-
-# Now, filter by missing data. Only retain sites present in at least 95%, 90%, 85% of individuals 
-
-
-
-
-#vcftools --gvcf MEVE_SNPs_filter_depth.vcf --remove-indels --max-missing 0.95 --recode --stdout > Filtered/MEVE_SNPs_filter_depth_m95.vcf # 48972 sites remaining
-#vcftools --gvcf MEVE_variants_unfiltered.vcf.gz --remove-indels --max-missing 0.90 --recode --stdout > Filtered/MEVE_SNPs_filter_depth_m90.vcf
-#vcftools --gvcf MEVE_variants_unfiltered.vcf.gz --remove-indels --max-missing 0.85 --recode --stdout > Filtered/MEVE_SNPs_filter_depth_m85.vcf
-
 # gatk IndexFeatureFile \
 #     -I MEVE_variants_unfiltered.vcf.gz
-
 
 # gatk VariantFiltration -V MEVE_variants_unfiltered.vcf.gz  --filter-expression "QD < 2.0" --filter-name "QD2" \
 #  --filter-expression "QUAL < 30.0" --filter-name "QUAL30" \
@@ -265,9 +173,8 @@ cd $OUTDIR/GATK/GenotypeGVCFs
 #  --filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
 #  -O Filtered/MEVE_SNPs.filtered.vcf.gz
 
-
 #ml  VCFtools/0.1.16-GCC-11.2.0
-#cd /scratch/crs12448/MEVE/GATK/GenotypeGVCFs/Filtered
+cd /scratch/crs12448/MEVE/GATK/GenotypeGVCFs
 
 # This filtering further requires that sites are SNPs only, marks genotypes as missing if any individual genotype has a depth of less than 10, and requires that sites being present in at least 90% of inviduals. 
 #vcftools --gzvcf MEVE_SNPs.filtered.vcf.gz --remove-indels --minDP 10 --maf 0.05  --max-missing 0.90 --recode --stdout > /scratch/crs12448/MEVE/GATK/GenotypeGVCFs/Filtered/MEVE_SNPs_filtered_013024.vcf
@@ -276,22 +183,24 @@ cd $OUTDIR/GATK/GenotypeGVCFs
 # and quality by depth instead of just quality as recommended by GATK Best Practiices for Hard Filtering Variants. I imagine thes
 # are the same SNPs as the previous analysis, just slightly fewer in number. 
 
-# gatk VariantFiltration -V MEVE_variants_unfiltered_allsites.vcf  --filter-expression "QD < 2.0" --filter-name "QD2" \
-#  --filter-expression "QUAL < 30.0" --filter-name "QUAL30" \
-#  --filter-expression "SOR > 3.0" --filter-name "SOR3" \
-#  --filter-expression "FS > 60.0" --filter-name "FS60" \
-#  --filter-expression "MQ < 40.0" --filter-name "MQ40" \
-#  --filter-expression "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
-#  --filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
-#  -O Filtered/MEVE_variants_filtered_allsites.vcf.gz
+##########################################################################################################################
+## For the all genes file with non-varient sites too - filter the same way as above
+# I am not sure for the strand bias filters whether they should be applied for invariant sites. From what 
+# I understand, invariant sites would not have strand bias because there is nothing to compare between
+# the reference and alternative alleles (only reads supporting the reference are present)
 
+# Regardless, I can later select variants based on specific filters if need. For example, only selecting variants
+# with QD > 2 PASS to use for downstream analyses
 
+gatk VariantFiltration -V MEVE_variants_unfiltered_allgenes.vcf  --filter-expression "QD < 2.0" --filter-name "QD2" \
+  --filter-expression "QUAL < 30.0" --filter-name "QUAL30" \
+  --filter-expression "SOR > 3.0" --filter-name "SOR3" \
+  --filter-expression "FS > 60.0" --filter-name "FS60" \
+  --filter-expression "MQ < 40.0" --filter-name "MQ40" \
+  --filter-expression "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
+  --filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
+  -O Filtered/MEVE_variants_filtered_allgenes.vcf.gz
 
-# # set filters
-# MAF=0.05
-# MISS=0.9
-# QUAL=30
-# MIN_DEPTH=10
 
 # cd $OUTDIR/GATK/GenotypeGVCFs/
 # # perform the filtering with vcftools
