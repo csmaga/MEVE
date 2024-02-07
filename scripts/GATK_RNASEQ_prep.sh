0b06ab7c196bbc47ce5a5471fbe0e9814f9cb836
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-02-07 15:36:07 -0500

    Update GATK_RNASEQ_prep.sh

--- scripts/GATK_RNASEQ_prep.sh
+++ scripts/GATK_RNASEQ_prep.sh
@@ -192,14 +192,22 @@ cd /scratch/crs12448/MEVE/GATK/GenotypeGVCFs
 # Regardless, I can later select variants based on specific filters if need. For example, only selecting variants
 # with QD > 2 PASS to use for downstream analyses
 
-gatk VariantFiltration -V MEVE_variants_unfiltered_allgenes.vcf  --filter-expression "QD < 2.0" --filter-name "QD2" \
-  --filter-expression "QUAL < 30.0" --filter-name "QUAL30" \
-  --filter-expression "SOR > 3.0" --filter-name "SOR3" \
-  --filter-expression "FS > 60.0" --filter-name "FS60" \
-  --filter-expression "MQ < 40.0" --filter-name "MQ40" \
-  --filter-expression "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
-  --filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
-  -O Filtered/MEVE_variants_filtered_allgenes.vcf.gz
+# gatk VariantFiltration -V MEVE_variants_unfiltered_allgenes.vcf  --filter-expression "QD < 2.0" --filter-name "QD2" \
+#   --filter-expression "QUAL < 30.0" --filter-name "QUAL30" \
+#   --filter-expression "SOR > 3.0" --filter-name "SOR3" \
+#   --filter-expression "FS > 60.0" --filter-name "FS60" \
+#   --filter-expression "MQ < 40.0" --filter-name "MQ40" \
+#   --filter-expression "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
+#   --filter-expression "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
+#   -O Filtered/MEVE_variants_filtered_allgenes.vcf.gz
+
+ gatk SelectVariants \
+     -R /scratch/crs12448/MEVE/Genome/Amiss_ref.fasta  \
+     -V Filtered/MEVE_variants_filtered_allgenes.vcf.gz \
+     --select-type-to-include SNP \
+     --exclude-filtered TRUE \
+     -O Filtered/MEVE_variants_filtered_allgenes_PASS.vcf
+
 
 
 # cd $OUTDIR/GATK/GenotypeGVCFs/
