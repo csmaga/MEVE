0d5bed1fe81c58bbe267507bb60402f2d6bd0b2a
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-02-07 15:37:44 -0500

    Update GATK_RNASEQ_prep.sh

--- scripts/GATK_RNASEQ_prep.sh
+++ scripts/GATK_RNASEQ_prep.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=300G
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j
 #SBATCH --mail-user=crs12448@uga.edu
 #SBATCH --mail-type=END,FAIL
