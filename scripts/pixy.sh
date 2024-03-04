3720e3a688c0f2451a73f73505c9ca0fa61d1780
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-03-04 16:30:51 -0500

    Create pixy.sh

---
+++ scripts/pixy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=Pixy
+#SBATCH --partition=highmem_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=150G
+#SBATCH --time=48:00:00
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j
+#SBATCH --mail-user=crs12448@uga.edu
+#SBATCH --mail-type=END,FAIL
+
+pixy --stats pi fst dxy \
+--vcf /scratch/crs12448/MEVE/GATK/GenotypeGVCFs/Filtered/MEVE_sites_pixy.vcf.gz \
+--populations /scratch/crs12448/MEVE/PopGen/MEVE_pops.txt \
+--n_cores 4 \
+--bed_file /scratch/crs12448/MEVE/PopGen/gene_bed_sorted.bed
