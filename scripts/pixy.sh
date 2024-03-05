321ac4939d7dcca9c71afbe11c125b86ed5d3a24
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-03-05 15:27:57 -0500

    Update pixy.sh

--- scripts/pixy.sh
+++ scripts/pixy.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Pixy
 #SBATCH --partition=highmem_p
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=12
 #SBATCH --mem=150G
 #SBATCH --time=48:00:00
 #SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j
@@ -11,12 +11,14 @@
 
 ml  Miniconda3/23.5.2-0
 ml VCFtools
+source ~/apps/eb/Miniconda3/23.5.2-0/etc/profile.d/conda.sh
+
 conda init bash
 conda activate /home/crs12448/conda_env
 
 pixy --stats pi fst dxy \
 --vcf /scratch/crs12448/MEVE/GATK/GenotypeGVCFs/Filtered/MEVE_sites_pixy.vcf.gz \
 --populations /scratch/crs12448/MEVE/PopGen/MEVE_pops.txt \
---n_cores 4 \
+--n_cores 12 \
 --bed_file /scratch/crs12448/MEVE/PopGen/gene_bed_sorted.bed \
 --output_folder /scratch/crs12448/MEVE/PopGen \
