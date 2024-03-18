8ced09d90e57a6b4067218670255a142f7010da5
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-03-18 16:14:05 -0400

    Create SNPGenie.sh

---
+++ scripts/SNPGenie.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=SNPGenie
+#SBATCH --partition=highmem_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100G
+#SBATCH --time=48:00:00
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j
+#SBATCH --mail-user=crs12448@uga.edu
+#SBATCH --mail-type=END,FAIL
+
+
+ml SNPGenie/1.0-foss-2022a-Perl-5.34.1
+
+snpgenie.pl --vcfformat=4 --gtffile Amiss.annot.2022.gtf --fastafile Amiss_ref.fasta 
+
