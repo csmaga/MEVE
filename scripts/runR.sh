cabe48d03a6d84aa994db38833f90117e02c6763
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2024-07-11 10:51:08 -0400

    Update runR.sh

--- scripts/runR.sh
+++ scripts/runR.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=highmem_p	                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
 #SBATCH --cpus-per-task=4		                            # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=300gb			                                # Total memory for job
+#SBATCH --mem=150gb			                                # Total memory for job
 #SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j	    # Standard output and error log - # replace cbergman with your myid
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
@@ -15,4 +15,4 @@ ml R/4.3.1-foss-2022a
 
 export R_LIBS=/home/crs12448/R/x86_64-pc-linux-gnu-library/4.3
 
-R CMD BATCH ~/MEVE/WGCNA_networks_cluster.R
+R CMD BATCH ~/GenomicFeatures_count_reads.R
