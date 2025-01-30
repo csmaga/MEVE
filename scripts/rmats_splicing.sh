bd4b8e48387c40dcd35b31c74690872138f52004
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2025-01-30 12:46:16 -0500

    Update rmats_splicing.sh

--- scripts/rmats_splicing.sh
+++ scripts/rmats_splicing.sh
@@ -11,4 +11,4 @@
 
 ml rMATS-turbo/4.1.1-foss-2020b
 
-rmats.py --b1 /scratch/crs12448/MEVE/rmats/southern_pops.txt --b2 /scratch/crs12448/MEVE/rmats/northern_pops.txt --gtf /scratch/crs12448/GATOR_GENOME/Amiss.annot.2022.gff -t paired --readLength 150 --libType fr-firststrand --nthread 8 --od /scratch/crs12448/MEVE/rmats/results --tmp /scratch/crs12448/MEVE/rmats/temp
+rmats.py --b1 /scratch/crs12448/MEVE/rmats/southern_pops.txt --b2 /scratch/crs12448/MEVE/rmats/northern_pops.txt --gtf /scratch/crs12448/GATOR_GENOME/Amiss.annot.2022.gff -t paired --readLength 150 --libType fr-firststrand --od /scratch/crs12448/MEVE/rmats/results --tmp /scratch/crs12448/MEVE/rmats/temp
