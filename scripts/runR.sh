ac1ad2418eeef73fc351956e1d22940ca8b4ef32
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2025-02-03 10:36:09 -0500

    Update runR.sh

--- scripts/runR.sh
+++ scripts/runR.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=highmem_p	                            # Partition (queue) name
 #SBATCH --ntasks=1			                                # Single task job
 #SBATCH --cpus-per-task=8		                            # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=400gb			                                # Total memory for job
+#SBATCH --mem=300gb			                                # Total memory for job
 #SBATCH --time=48:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j	    # Standard output and error log - # replace cbergman with your myid
 #SBATCH --mail-user=christopher.smaga@uga.edu                    # Where to send mail - # replace cbergman with your myid
