9eb35c57138980ba45e8bdcd8ca9d68d8ca0c86d
Chris Smaga <70600001+csmaga@users.noreply.github.com>
2025-02-05 11:01:45 -0500

    Create subset_bams.sh

---
+++ scripts/subset_bams.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=bam_subset
+#SBATCH --partition=highmem_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=300G
+#SBATCH --time=24:00:00
+#SBATCH --output=/scratch/crs12448/MEVE/Logs/log.%j
+#SBATCH --mail-user=crs12448@uga.edu
+#SBATCH --mail-type=END,FAIL
+
+
+cd /scratch/crs12448/MEVE/Alignment/HISAT2/BAM
+ml SAMtools/1.18-GCC-12.3.0
+
+for file in *.bam; do
+  samtools view ${file} "NW_017708899.1:4382450-4395344" > /scratch/crs12448/MEVE/Alignment/HISAT2/BAM/gene_bams/aig1_${file}
+done
